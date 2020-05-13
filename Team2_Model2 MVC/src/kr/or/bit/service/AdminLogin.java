6d98d59891f7ad03420eaf59e44a5818399a3182
Minep <pmh@ge.kornu.ac.kr>
2020-05-13 17:39:36 +0900

    service 2020 05 13 민혜

---
+++ Team2_Model2 MVC/src/kr/or/bit/service/AdminLogin.java
@@ -0,0 +1,17 @@
+package kr.or.bit.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.bit.action.Action;
+import kr.or.bit.action.ActionForward;
+
+public class AdminLogin implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
